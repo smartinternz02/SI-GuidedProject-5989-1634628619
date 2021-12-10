bb9d3b26950548abe3f88e32eb0c28c2aff9926b
arathyts <90604085+arathyts@users.noreply.github.com>
2021-12-10 10:58:12 +0530

    Add files via upload

---
+++ app_ibm.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 27 08:57:55 2021
+
+@author: rincy
+"""
+
+#import the necessary packages
+import pandas as pd
+import numpy as np
+import pickle
+import os
+import requests
+from flask import Flask, request, render_template
+
+
+
+# NOTE: you must manually set API_KEY below using information retrieved from your IBM Cloud account.
+API_KEY = "QIZb5MLZY-bqVVWxx1Jf5gqGSHJs9JI12pXkocNyZXJq"
+token_response = requests.post('https://iam.cloud.ibm.com/identity/token', data={"apikey": API_KEY, "grant_type": 'urn:ibm:params:oauth:grant-type:apikey'})
+mltoken = token_response.json()["access_token"]
+
+header = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + mltoken}
+
+app=Flask(__name__,template_folder="templates")
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('home.html')
+@app.route('/home', methods=['GET'])
+def about():
+    return render_template('home.html')
+@app.route('/pred',methods=['GET'])
+def page():
+    return render_template('upload.html')
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    print("[INFO] loading model...")
+#    model = pickle.load(open('fdemand.pkl', 'rb'))
+    input_features = [float(x) for x in request.form.values()]
+    features_value = [np.array(input_features)]
+    print(features_value)
+    
+    #features_name = ['homepage_featured', 'emailer_for_promotion', 'op_area', 'cuisine',
+       #'city_code', 'region_code', 'category']
+    payload_scoring = {"input_data":[{"field": [['homepage_featured', 'emailer_for_promotion', 'op_area', 'cuisine','city_code', 'region_code', 'category']], "values": [input_features]}]}
+    response_scoring = requests.post('https://us-south.ml.cloud.ibm.com/ml/v4/deployments/08405343-1091-4c04-8997-103f6ed74efa/predictions?version=2021-11-17', json=payload_scoring, headers={'Authorization': 'Bearer ' + mltoken})
+    print("Scoring response")
+    print(response_scoring.json())
+    predictions =response_scoring.json()
+    print(predictions)
+    print('Final Prediction Result',predictions['predictions'][0]['values'][0][0])
+    pred = predictions['predictions'][0]['values'][0][0]
+    print(pred)
+    return render_template('upload.html', prediction_text=pred)
+
+    
+if __name__ == '__main__':
+      app.run(debug=False)
+ 
